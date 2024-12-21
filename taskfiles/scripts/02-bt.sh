a2844218cf99af0bd267502d9f317cad26242735
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-12-21 20:32:31 +0100

    chore: Add bluetooth installation

---
+++ taskfiles/scripts/02-bt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -uo pipefail
+
+PKGS=(
+  # https://wiki.archlinux.org/title/Bluetooth
+  bluez
+  bluez-utils
+  bluez-hid2hci
+)
+
+echo "Installing wireless and bluetooth related packages"
+
+for PKG in "${PKGS[@]}"; do
+  echo "Installing package: $PKG"
+  sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+echo "Setting wireless and bluetooth services"
+
+sudo systemctl enable --now bluetooth.service
+
+echo "Wireless and bluetooth setup is complete"

37e5579c913013fabc1c04494510f0e44671725a
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-12-22 00:07:38 +0100

    chore: introduce yay

---
+++ taskfiles/scripts/06-hypr-yay.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -uo pipefail
+
+PKGS=(
+    hyprland-qtutils
+)
+
+echo "Installing hypr AUR packages"
+
+for PKG in "${PKGS[@]}"; do
+    echo "Installing package: $PKG"
+    yay -S "$PKG" --noconfirm --needed
+done

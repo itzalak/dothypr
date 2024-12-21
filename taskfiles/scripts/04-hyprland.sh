815a849c6c5c9d10749a20671685888bd63fb8f5
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-12-21 22:52:25 +0100

    chore: first steps into hyprland and waybar

---
+++ taskfiles/scripts/04-hyprland.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -uo pipefail
+
+PKGS=(
+    hyprland
+    dunst
+    kitty
+    dolphin
+    wofi
+    xdg-desktop-portal-hyprland
+    qt5-wayland
+    qt6-wayland
+    polkit-kde-agent
+    grim
+    slurp
+    # Others
+    waybar
+)
+
+echo "Installing fonts"
+
+for PKG in "${PKGS[@]}"; do
+    echo "Installing package: $PKG"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+echo "Fonts installed"

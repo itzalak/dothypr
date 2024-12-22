75328d16d2cd4e19675c4036a9033b6911352be6
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-12-22 12:39:02 +0100

    chore: start nvidia trials

--- taskfiles/scripts/10-nvidia.sh
+++ taskfiles/scripts/10-nvidia.sh
@@ -5,12 +5,12 @@ set -uo pipefail
 PKGS=(
     # Nvidia
     # https://wiki.hyprland.org/Nvidia/
-    # dkms
-    # nvidia-open
-    # nvidia-open-dkms
-    # nvidia-utils
-    # lib32-nvidia-utils
-    # egl-wayland
+    dkms
+    nvidia-open
+    nvidia-open-dkms
+    nvidia-utils
+    lib32-nvidia-utils
+    egl-wayland
 )
 
 echo "Installing packages"
@@ -19,3 +19,5 @@ for PKG in "${PKGS[@]}"; do
     echo "Installing package: $PKG"
     sudo pacman -S "$PKG" --noconfirm --needed
 done
+
+echo "Manual steps required for successfull installation. please update mkinitcpio and modprobe"

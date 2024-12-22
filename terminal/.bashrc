891fe72e953d4f8e94a196b9c627089d40321f92
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-12-22 18:36:59 +0100

    chore: Add python and sdkman installation

--- terminal/.bashrc
+++ terminal/.bashrc
@@ -33,3 +33,15 @@ eval "$(zoxide init bash)"
 
 # Starship
 eval "$(starship init bash)"
+
+# Python
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - path)"
+
+# Created by `pipx`
+export PATH="$PATH:$HOME/.local/bin"
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
--- terminal/.config/fish/config.fish
+++ terminal/.config/fish/config.fish
@@ -5,6 +5,26 @@ end
 set -g -x fish_greeting ''
 set --universal --export STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
 
+fish_add_path --prepend "$HOME/.local/bin"
+if test -d "$HOME/.pyenv"
+    set --universal --export PYENV_ROOT $HOME/.pyenv
+    fish_add_path --prepend "$PYENV_ROOT/bin"
+     pyenv init - | source
+end
+
+if test -d "$HOME/.sdkman"
+    set --universal --export SDKMAN_DIR "$HOME/.sdkman"
+    if set -q SDKMAN_DIR; and not test -f "$SDKMAN_DIR/bin/sdkman-init.sh"
+        echo "WARNING: SDKMAN! installation path set to $SDKMAN_DIR, but no installation found there"
+        exit 0
+    end
+    fish_add_path --prepend (find "$HOME/.sdkman/candidates/gradle/current/bin" -maxdepth 0)
+    fish_add_path --prepend (find "$HOME/.sdkman/candidates/java/current/bin" -maxdepth 0)
+    fish_add_path --prepend (find "$HOME/.sdkman/candidates/kotlin/current/bin" -maxdepth 0)
+    fish_add_path --prepend (find "$HOME/.sdkman/candidates/maven/current/bin" -maxdepth 0)
+    fish_add_path --prepend (find "$HOME/.sdkman/candidates/groovy/current/bin" -maxdepth 0)
+end
+
 # End of file required
 starship init fish | source
 

8064faada77c6a8c29fcfdff033eaa40eeaa13e8
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-12-31 12:10:21 +0100

    chore: tweak yay installation

--- taskfiles/scripts/05-yay.sh
+++ taskfiles/scripts/05-yay.sh
@@ -14,6 +14,10 @@ git clone https://aur.archlinux.org/yay.git "$YAY_DIR"
 # Navigate to the directory and run makepkg -si
 cd "$YAY_DIR" && makepkg -si
 
+# Enable development packages update
+# https://github.com/Jguer/yay?tab=readme-ov-file#first-use
+yay -Y --devel --save
+
 echo
 echo "Yay setup successfully"
 echo

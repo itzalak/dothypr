00080bf88dee511de4702760734140da53248ea0
Itzalak <24299428+itzalak@users.noreply.github.com>
2025-01-14 22:45:19 +0100

    chore: some docs

--- taskfiles/scripts/50-gnome.sh
+++ taskfiles/scripts/50-gnome.sh
@@ -3,6 +3,9 @@
 set -uo pipefail
 
 PKGS=(
+	# Gnome by default installs a lot of packages unnecessary to me, its possible I remove some
+	# Examples: epiphany, gnome-contacts, malcontent, gnome-tour
+	# Others might not be possible, like: nautilus
 	gnome
 	gnome-tweaks
 	xclip

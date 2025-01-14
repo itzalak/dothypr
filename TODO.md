b1432c32e672f13dda98d55ab18d54ffea9a2830
Itzalak <24299428+itzalak@users.noreply.github.com>
2025-01-14 21:37:31 +0100

    chore: another attempt, now gnome first

---
+++ TODO.md
@@ -0,0 +1,15 @@
+# TODO
+
+- clipboard
+- clipboard manager
+- xrandr/arandr
+- figure out the hardware
+
+## Gnome
+
+- Keybindings (maybe sxhkd)
+
+## hyprland
+
+- Try hyprland again
+- Check for conflicts with gnome installation
--- taskfiles/scripts/50-gnome.sh
+++ taskfiles/scripts/50-gnome.sh
@@ -3,22 +3,9 @@
 set -uo pipefail
 
 PKGS=(
-	baobab
-	gnome-calculator
-	gnome-characters
-	gnome-control-center
-	gnome-disk-utility
-	gnome-font-viewer
-	gnome-keyring
-	gnome-logs
-	gnome-menus
-	gnome-session
-	gnome-settings-daemon
-	gnome-shell
-	gnome-shell-extensions
-	xdg-desktop-portal-gnome
-	xdg-user-dirs-gtk
-	seahorse
+	gnome
+	gnome-tweaks
+	xclip
 )
 
 echo "Installing packages"
@@ -26,6 +13,19 @@ echo "Installing packages"
 for PKG in "${PKGS[@]}"; do
 	echo "Installing package: $PKG"
 	sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+YAY_PKGS=(
+	whitesur-gtk-theme
+	whitesur-icon-theme
+
+)
+
+echo "Installing yay packages"
+
+for YAY in "${YAY_PKGS[@]}"; do
+	echo "Installing package: $YAY"
+	yay -S "$YAY" --noconfirm --needed
 done
 
 echo "Packages installed"
